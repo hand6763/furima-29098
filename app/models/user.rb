328baf1a550eceee4120ef99e45dd7f529174b1d
hand6763 <yhand6763@gmail.com>
2020-12-01 23:33:49 +0900

    バリデーションwith_option導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
   validates :birthday, presence: true
-  validates :sei, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
-  validates :mei, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
-  validates :sei_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" }
-  validates :mei_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" }
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" } do
+      validates :sei
+      validates :mei
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" } do  
+      validates :sei_kana
+      validates :mei_kana
+    end
+  
+  end
+
   validates :email, uniqueness: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)\w{6}\z/, message: "Include both letters and numbers" }
 end
