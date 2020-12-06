676de8085365b6459bbd11fed2f23fc79f904f3f
hand6763 <yhand6763@gmail.com>
2020-12-06 18:58:33 +0900

    active_hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
   belongs to :users
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :deliver_fee
+  belongs_to :shiooing_area
+  belongs_to :shiooing_day
+  belongs_to :status
+
 end
