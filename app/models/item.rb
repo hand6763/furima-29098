ff2252d1d3ff82e79a7f7d4e243e8720db9485e8
hand6763 <yhand6763@gmail.com>
2020-12-13 16:34:45 +0900

    rubocop実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,6 @@ class Item < ApplicationRecord
 
   validates :price, numericality: { message: 'Half-width number' }
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
-  
 
   with_options numericality: { other_than: 1, message: 'Select' } do
     validates :category_id
