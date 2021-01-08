7b27b47d19e9a310aa9d8bdd770aeb6def8f32b7
hand6763 <yhand6763@gmail.com>
2021-01-08 09:59:25 +0900

    テストコード実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'handa' && password == '0520'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 
